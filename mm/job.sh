2c90bc8b3af16031afded599b3bc1a0adb5d2e45
kazuki <kaz.tokyo.tech20@gmail.com>
2024-05-04 15:59:29 +0900

    style: format

--- mm/job.sh
+++ mm/job.sh
@@ -4,3 +4,12 @@
 #$ -l h_rt=00:10:00
 
 ./mm 2000 2000 2000
+
+# naive cpu
+#  ./mm 2000 2000 2000
+# Matmul took 2246009 us --> 7.124 GFlops
+# Matmul took 2237316 us --> 7.151 GFlops
+# Matmul took 2244341 us --> 7.129 GFlops
+# Matmul took 2252441 us --> 7.103 GFlops
+# Matmul took 2239994 us --> 7.143 GFlops
+
